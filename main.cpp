4a16dfb4e07e141662513ca5a043a08c7ea809a0
Michael Giambalvo <heathkit@gmail.com>
2015-06-12 10:07:38 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "mbed.h"
+
+DigitalOut myled(LED1);
+
+int main() {
+    while(1) {
+        myled = 1; // LED is ON
+        wait(0.5); // 200 ms
+        myled = 0; // LED is OFF
+        wait(1.0); // 1 sec
+        printf("Flash!\n\r");
+    }
+}
